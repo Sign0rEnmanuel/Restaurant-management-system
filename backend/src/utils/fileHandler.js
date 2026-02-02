05c8c3ab0ce7150a3d541d6a05e3bb46da0e164e
Enmanuel <marzalenmanuel4@gmail.com>
2026-02-02 11:03:46 -0300

    Fix a bug in fileHandler.js about async

--- backend/src/utils/fileHandler.js
+++ backend/src/utils/fileHandler.js
@@ -5,21 +5,21 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const readJSON = (filename) => {
+export const readJSON = async (filename) => {
     try {
         const filePath = path.join(__dirname, '../database', filename);
-        const data = fs.readFile(filePath, 'utf8');
+        const data = await fs.readFile(filePath, 'utf8');
         return JSON.parse(data);
     } catch (error) {
         console.log(`Error reading file ${filename}: ${error}`);
-        return false;
+        return [];
     }
 };
 
-export const writeJSON = (filename, data) => {
+export const writeJSON = async (filename, data) => {
     try {
         const filePath = path.join(__dirname, '../database', filename);
-        fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
         return true;
     } catch (error) {
         console.log(`Error writing file ${filename}: ${error}`);
